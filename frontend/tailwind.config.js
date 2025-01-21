ef417fb1b282f98df2534ffa7eb753f9315e20e5
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2025-01-21 11:41:02 +0300

    add changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        'very-light-yellow': '#FFFFE0', // Very light shade of yellow
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
